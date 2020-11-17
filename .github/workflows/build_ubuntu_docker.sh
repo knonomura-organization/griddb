1d752269c171227e81b135478941e87f4f42d51a
Dang Trung Tin <tin.dangtrung@toshiba-tsdv.com>
2020-11-17 16:44:56 +0700

    Update

--- .github/workflows/build_ubuntu_docker.sh
+++ .github/workflows/build_ubuntu_docker.sh
@@ -5,7 +5,7 @@ docker pull ubuntu:18.04
 docker run --name ${DOCKER_CONTAINER_NAME_CENTOS} -ti -d -v `pwd`:/griddb --env GS_LOG=/griddb/log --env GS_HOME=/griddb ubuntu:18.04
 #install dependency, support for griddb sever
 docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -xec "apt-get update"
-docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -xec "apt-get install -y debhelper libz-dev. gcc-4.8 g++-4.8"
+docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -xec "apt-get install -y debhelper libz-dev gcc-4.8 g++-4.8"
 
 docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -xec "ln -sf /usr/bin/gcc-4.8 /usr/bin/gcc"
 docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -xec "ln -sf /usr/bin/g++-4.8 /usr/bin/g++"
