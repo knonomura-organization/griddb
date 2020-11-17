44aee0ec5b09b07d4e4f91d6415b32da4415460b
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-11-17 15:47:10 +0700

    Add build for ubuntu

--- .github/workflows/build_ubuntu_docker.sh
+++ .github/workflows/build_ubuntu_docker.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -xe
 
 # Build package on ubuntu
-apt-get install debhelper
-dpkg-buildpackage -b
+docker pull ubuntu:18.04
+docker run --name ${DOCKER_CONTAINER_NAME_CENTOS} -ti -d -v `pwd`:/griddb --env GS_LOG=/griddb/log --env GS_HOME=/griddb centos:centos${CENTOS_VERSION}
+#install dependency, support for griddb sever
+docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -xec "apt-get install debhelper"
+
+#config sever
+docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -c "cd griddb \
+&& dpkg-buildpackage -b"
+
+
