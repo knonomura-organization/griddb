c8393f9fef4068da1dbcfac00fa142c900588baf
Dang Trung Tin <tin.dangtrung@toshiba-tsdv.com>
2020-11-18 12:01:10 +0700

    update

--- .github/workflows/build_ubuntu_docker.sh
+++ .github/workflows/build_ubuntu_docker.sh
@@ -3,13 +3,9 @@
 sudo apt-get install gcc-4.8
 sudo apt-get install g++-4.8
 sudo apt-get install tcl debhelper libz-dev libsqlite3-dev
-
 export CC=gcc-4.8
 export CC_FOR_BUILD=gcc-4.8
 export CXX=g++-4.8
 export CXX_FOR_BUILD=g++-4.8
-#./bootstrap.sh
-#./configure
-#make
 dpkg-buildpackage -b
 
