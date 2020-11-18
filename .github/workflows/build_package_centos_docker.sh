6257ec5e7bac43c87c80ee127294da558179a937
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-11-18 18:00:20 +0700

    Update build package centos

--- .github/workflows/build_package_centos_docker.sh
+++ .github/workflows/build_package_centos_docker.sh
@@ -5,7 +5,7 @@ docker pull centos:centos${CENTOS_VERSION}
 docker run --name ${DOCKER_CONTAINER_NAME_CENTOS} -ti -d -v `pwd`:/griddb --env GS_LOG=/griddb/log --env GS_HOME=/griddb centos:centos${CENTOS_VERSION}
 
 #install dependency, support for griddb sever
-docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -xec "yum install automake make gcc gcc-c++ libpng-devel java ant zlib-devel tcl.x86_64 -y"
+docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -xec "yum install automake make gcc gcc-c++ libpng-devel java ant zlib-devel tcl.x86_64 zip unzip -y"
 
 echo $(grep -Eo '[0-9\.]+' installer/SPECS/griddb.spec) >output.txt
 export GRIDDB_VERSION=$(awk '{print $1}' output.txt)
