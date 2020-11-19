5f3db2d6642f8bac63fdaedcc8206bb3fbe53574
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-11-19 13:54:45 +0700

    Update

--- .github/workflows/build_package_centos_docker.sh
+++ .github/workflows/build_package_centos_docker.sh
@@ -33,6 +33,6 @@ docker exec -e GRIDDB_VERSION="$GRIDDB_VERSION" -e GRIDDB_FOLDER_NAME="$GRIDDB_F
 && echo $PWD    \
 && pwd \
 && ls SOURCES  \
-&& realpath  griddb/installer/SOURCES/$GRIDDB_ZIP_FILE \
+&& realpath  SOURCES/$GRIDDB_ZIP_FILE \
 && rpmbuild --define=\"_topdir /home/runner/work/griddb/installer\" -bb --clean SPECS/griddb.spec"
 
