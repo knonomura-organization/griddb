f3c927d55cb43a9079bb88780e24d9ac796cd8f7
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-11-19 14:56:20 +0700

    Update

--- .github/workflows/build_package_centos_docker.sh
+++ .github/workflows/build_package_centos_docker.sh
@@ -37,3 +37,7 @@ docker exec -e GRIDDB_VERSION="$GRIDDB_VERSION" -e GRIDDB_FOLDER_NAME="$GRIDDB_F
 && realpath  SOURCES/$GRIDDB_ZIP_FILE \
 && rpmbuild --define=\"_topdir /griddb/installer\" -bb --clean SPECS/griddb.spec"
 
+# Copy rpm file to host
+docker cp ${DOCKER_CONTAINER_NAME_CENTOS}:/griddb/installer/RPMS/x86_64/griddb-*.rpm .
+
+
