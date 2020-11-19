c86f57fb1533a4b7351839ad5c54a356327e55f7
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-11-19 09:24:27 +0700

    Update

--- .github/workflows/build_package_centos_docker.sh
+++ .github/workflows/build_package_centos_docker.sh
@@ -1,12 +1,19 @@
 #!/bin/sh -xe
 
-#pull and run image docker centos7
+# Pull and run image docker centos7
 docker pull centos:centos${CENTOS_VERSION}
 docker run --name ${DOCKER_CONTAINER_NAME_CENTOS} -ti -d -v `pwd`:/griddb --env GS_LOG=/griddb/log --env GS_HOME=/griddb centos:centos${CENTOS_VERSION}
 
-#install dependency, support for griddb sever
+# Install dependency, support for griddb sever
 docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash -xec "yum install automake make gcc gcc-c++ libpng-devel java ant zlib-devel tcl.x86_64 zip unzip rpm-build -y"
 
+# Build
+docker exec ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash  -c "cd griddb \
+&& ./bootstrap.sh \
+&& ./configure \
+&& make"
+
+# Get griddb version and set source code zip file name, ex "4.5.2" and "griddb-4.5.2.zip"
 echo $(grep -Eo '[0-9\.]+' installer/SPECS/griddb.spec) >output.txt
 export GRIDDB_VERSION=$(awk '{print $1}' output.txt)
 echo $GRIDDB_VERSION
@@ -16,13 +23,8 @@ export GRIDDB_ZIP_FILE="${GRIDDB_FOLDER_NAME}.zip"
 echo $GRIDDB_ZIP_FILE
 rm output.txt
 
-#config sever
-docker exec -e GRIDDB_VERSION="$GRIDDB_VERSION" -e GRIDDB_FOLDER_NAME="$GRIDDB_FOLDER_NAME" -e  GRIDDB_ZIP_FILE="$GRIDDB_ZIP_FILE" ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash  -c "cd griddb \
-&& ./bootstrap.sh \
-&& ./configure \
-&& make \
-&& cd ../    \
-&& cp -rf griddb/ $GRIDDB_FOLDER_NAME    \
+# Create rpm file
+docker exec -e GRIDDB_VERSION="$GRIDDB_VERSION" -e GRIDDB_FOLDER_NAME="$GRIDDB_FOLDER_NAME" -e  GRIDDB_ZIP_FILE="$GRIDDB_ZIP_FILE" ${DOCKER_CONTAINER_NAME_CENTOS} /bin/bash  -c "cp -rf griddb/ $GRIDDB_FOLDER_NAME    \
 && zip -r $GRIDDB_ZIP_FILE $GRIDDB_FOLDER_NAME   \
 && mv $GRIDDB_ZIP_FILE griddb/installer/SOURCES/  \
 && rm -rf $GRIDDB_FOLDER_NAME   \
