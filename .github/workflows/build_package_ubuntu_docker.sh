31479879426c72fb9c36ae17ae9ca130d385e492
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-11-20 10:42:28 +0700

    Update

---
+++ .github/workflows/build_package_ubuntu_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -xe
+
+sudo apt-get install gcc-4.8
+sudo apt-get install g++-4.8
+sudo apt-get install tcl debhelper libz-dev libsqlite3-dev
+export CC=gcc-4.8
+export CC_FOR_BUILD=gcc-4.8
+export CXX=g++-4.8
+export CXX_FOR_BUILD=g++-4.8
+dpkg-buildpackage -b
+
+# Install package
+sudo apt-get installl ../griddb_*_amd64.deb
+
+
+
