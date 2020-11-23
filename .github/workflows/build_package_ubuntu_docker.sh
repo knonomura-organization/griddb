1ae3bbe3e634d89630de5bc081f5a8005c53eccc
Dang Trung Tin <tin.dangtrung@toshiba-tsdv.com>
2020-11-23 13:45:00 +0700

    Update start griddb after publish

--- .github/workflows/build_package_ubuntu_docker.sh
+++ .github/workflows/build_package_ubuntu_docker.sh
@@ -12,5 +12,8 @@ dpkg-buildpackage -b
 # Install package
 sudo apt-get install ../griddb_*_amd64.deb
 
+su - gsadm -c "gs_passwd admin -p admin"
 
-
+sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"dockerGridDB\"/g \
+/var/lib/gridstore/conf/gs_cluster.json
+su -c "gs_startnode -w -u admin/admin; gs_joincluster -c dockerGridDB -u admin/admin" - gsadm
