bf9c1a5300042407e3a7451fd0812b958c42654e
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-11-24 17:30:58 +0700

    Update

--- .github/workflows/build_package_ubuntu.sh
+++ .github/workflows/build_package_ubuntu.sh
@@ -13,7 +13,7 @@ export CXX_FOR_BUILD=g++-4.8
 dpkg-buildpackage -b
 
 # Install package and config GridDB server
-sudo dpkg -i install ../griddb_*_amd64.deb
+sudo dpkg -i ../griddb_*_amd64.deb
 sudo env GRIDDB_PASSWORD="$GRIDDB_PASSWORD" su - gsadm -c "gs_passwd ${GRIDDB_USERNAME} -p ${GRIDDB_PASSWORD}"
 sudo env GRIDDB_SERVER_NAME="$GRIDDB_SERVER_NAME" sed -i -e s/\"clusterName\":\"\"/\"clusterName\":\"${GRIDDB_SERVER_NAME}\"/g \
 /var/lib/gridstore/conf/gs_cluster.json
