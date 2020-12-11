7e4c8ed558abb7f2ab2d529f7a4d172a321724db
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-12-11 10:58:36 +0700

    Update

--- .github/workflows/script/centos/dislay_package_information.sh
+++
@@ -1,7 +0,0 @@
-# Get griddb version and set source code zip file name, ex "4.5.2" and "griddb-4.5.2.zip"
-echo $(grep -Eo '[0-9\.]+' installer/SPECS/griddb.spec) > output.txt
-export GRIDDB_VERSION=$(awk '{print $1}' output.txt)
-rm output.txt
-
-# Check package information
-rpm -qip installer/RPMS/x86_64/griddb-$GRIDDB_VERSION-linux.x86_64.rpm
