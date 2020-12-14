029e7933fe6306856008b0c0b00cb7370424d335
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-12-14 13:58:16 +0700

    Update

--- .github/workflows/script/function.sh
+++ .github/workflows/script/function.sh
@@ -25,12 +25,9 @@ function build_rpm {
 
 function build_griddb {
     # Build GridDB server
-    ls
-    #cd griddb
     ./bootstrap.sh
     ./configure
     make
-    #cd ../
 }
 
 function install_griddb {
@@ -55,4 +52,14 @@ function run_sample {
     local username=$4
     local password=$5
     java gsSample/Sample1 $notification_host $notification_port $cluster_name $username $password
+}
+
+function opensuse_change_package_name {
+    # Get griddb version and set source code zip file name, ex "4.5.2" and "griddb-4.5.2.zip"
+    echo $(grep -Eo '[0-9\.]+' installer/SPECS/griddb.spec) > output.txt
+    export GRIDDB_VERSION=$(awk '{print $1}' output.txt)
+    rm output.txt
+
+    # Change file name to distinguish with CentOS package
+    mv installer/RPMS/x86_64/griddb-$GRIDDB_VERSION-linux.x86_64.rpm installer/RPMS/x86_64/griddb-$GRIDDB_VERSION-opensuse.x86_64.rpm
 }
