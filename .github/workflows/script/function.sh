3d8490c0be5c24bcb50596a099b39afa411cc0cd
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-12-16 18:41:15 +1000

    Update

--- .github/workflows/script/function.sh
+++ .github/workflows/script/function.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 
+get_version() {
+    if [ ! -f installer/SPECS/griddb.spec ]; then
+        echo "Spec file not found!"
+    fi
+
+    echo $(grep -Eo '[0-9\.]+' installer/SPECS/griddb.spec) > output.txt
+    local griddb_version=$(awk '{print $1}' output.txt)
+    rm output.txt
+    echo $griddb_version
+}
+
 build_rpm() {
     # Get griddb version and set source code zip file name, ex "4.5.2" and "griddb-4.5.2.zip"
-    echo $(grep -Eo '[0-9\.]+' installer/SPECS/griddb.spec) > output.txt
-    export GRIDDB_VERSION=$(awk '{print $1}' output.txt)
-    echo $GRIDDB_VERSION
-    export GRIDDB_FOLDER_NAME="griddb-${GRIDDB_VERSION}"
-    echo $GRIDDB_FOLDER_NAME
-    export GRIDDB_ZIP_FILE="${GRIDDB_FOLDER_NAME}.zip"
-    echo $GRIDDB_ZIP_FILE
-    rm output.txt
+    local griddb_version=$(get_version)
+    local griddb_folder_name="griddb-${griddb_version}"
+    local griddb_zip_file="${griddb_folder_name}.zip"
 
     # Create rpm file
     cd ..
-    cp -rf griddb/ $GRIDDB_FOLDER_NAME
-    rm -r $GRIDDB_FOLDER_NAME/.git
-    zip -r $GRIDDB_ZIP_FILE $GRIDDB_FOLDER_NAME
-    cp $GRIDDB_ZIP_FILE griddb/installer/SOURCES/
-    rm -rf $GRIDDB_FOLDER_NAME
+    cp -rf griddb/!(.git) $griddb_folder_name
+    #rm -r $griddb_folder_name/.git
+    zip -r $griddb_zip_file $GRIDDB_FOLDER_NAME
+    cp $griddb_zip_file griddb/installer/SOURCES/
+    rm -rf $griddb_folder_name
     cd griddb/installer
     rpmbuild --define="_topdir `pwd`" -bb --clean SPECS/griddb.spec
     cd ../..
@@ -31,19 +37,19 @@ build_griddb() {
 }
 
 install_griddb() {
-    # Get griddb version and set source code zip file name, ex "4.5.2" and "griddb-4.5.2.zip"
-    echo $(grep -Eo '[0-9\.]+' installer/SPECS/griddb.spec) > output.txt
-    export GRIDDB_VERSION=$(awk '{print $1}' output.txt)
-    rm output.txt
+    local griddb_version=$(get_version)
 
     # Install package
-    rpm -ivh installer/RPMS/x86_64/griddb-$GRIDDB_VERSION-linux.x86_64.rpm
+    rpm -ivh installer/RPMS/x86_64/griddb-$griddb_version-linux.x86_64.rpm
 }
 
 run_sample() {
     # Run sample
     export CLASSPATH=${CLASSPATH}:/usr/share/java/gridstore.jar
     mkdir gsSample
+    if [ ! -f /usr/griddb-*/docs/sample/program/Sample1.java ]; then
+        echo "Sample1.java not found!"
+    fi
     cp /usr/griddb-*/docs/sample/program/Sample1.java gsSample/.
     javac gsSample/Sample1.java
     local notification_host=$1
@@ -55,12 +61,12 @@ run_sample() {
 }
 
 opensuse_change_package_name() {
-    # Get griddb version and set source code zip file name, ex "4.5.2" and "griddb-4.5.2.zip"
-    echo $(grep -Eo '[0-9\.]+' installer/SPECS/griddb.spec) > output.txt
-    export GRIDDB_VERSION=$(awk '{print $1}' output.txt)
-    rm output.txt
+    local griddb_version=$(get_version)
 
     # Change file name to distinguish with CentOS package
-    mv installer/RPMS/x86_64/griddb-$GRIDDB_VERSION-linux.x86_64.rpm installer/RPMS/x86_64/griddb-$GRIDDB_VERSION-opensuse.x86_64.rpm
+    if [ ! -f installer/RPMS/x86_64/griddb-$griddb_version-linux.x86_64.rpm ]; then
+        echo "griddb-$griddb_version-linux.x86_64.rpm not found !"
+    fi
+    mv installer/RPMS/x86_64/griddb-$griddb_version-linux.x86_64.rpm installer/RPMS/x86_64/griddb-$GRIDDB_VERSION-opensuse.x86_64.rpm
 }
 
