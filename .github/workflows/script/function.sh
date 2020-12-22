f6bb2a33b795cd4c7d5ac7096f3a808835e253aa
Dang Trung Tin <tin1.dangtrung@toshiba.co.jp>
2020-12-22 17:22:28 +1000

    Update

--- .github/workflows/script/function.sh
+++ .github/workflows/script/function.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+readonly UBUNTU=Ubuntu
+readonly CENTOS=Centos
+readonly OPENSUSE=Opensuse
+
 get_version() {
     if [ ! -f installer/SPECS/griddb.spec ]; then
         echo "Spec file not found!"
@@ -11,29 +15,66 @@ get_version() {
     echo $griddb_version
 }
 
-build_rpm() {
-    # Get griddb version and set source code zip file name, 
-    #   ex "4.5.2" and "griddb-4.5.2.zip"
-    local griddb_version=$(get_version)
-    local griddb_folder_name="griddb-${griddb_version}"
-    local griddb_zip_file="${griddb_folder_name}.zip"
+build_package() {
+    local os=$1
 
-    # Create rpm file
-    cd ..
-    rsync -a --exclude=.git griddb/ $griddb_folder_name
-    zip -r $griddb_zip_file $griddb_folder_name
-    cp $griddb_zip_file griddb/installer/SOURCES/
-    rm -rf $griddb_folder_name
-    cd griddb/installer
-    rpmbuild --define="_topdir `pwd`" -bb --clean SPECS/griddb.spec
-    cd ../..
+    case os in
+        CENTOS | OPENSUSE)
+            # For CentOS and OpenSuse :
+
+            # Get griddb version and set source code zip file name,
+            #   ex "4.5.2" and "griddb-4.5.2.zip"
+            local griddb_version=$(get_version)
+            local griddb_folder_name="griddb-${griddb_version}"
+            local griddb_zip_file="${griddb_folder_name}.zip"
+
+            # Create rpm file
+            cd ..
+            rsync -a --exclude=.git griddb/ $griddb_folder_name
+            zip -r $griddb_zip_file $griddb_folder_name
+            cp $griddb_zip_file griddb/installer/SOURCES/
+            rm -rf $griddb_folder_name
+            cd griddb/installer
+            rpmbuild --define="_topdir `pwd`" -bb --clean SPECS/griddb.spec
+            cd ../..
+            ;;
+
+        UBUNTU)
+            dpkg-buildpackage -b
+            ;;
+
+        *)
+            echo -n "Unknown OS"
+            ;;
+
+    esac
+    # Change package name of OPENSUSE version to distinguish with CENTOS version
+    if ("$os" = OPENSUSE); then
+        $(opensuse_change_package_name)
+    fi
 }
 
 build_griddb() {
     # Build GridDB server
-    ./bootstrap.sh
-    ./configure
-    make
+    local os=$1
+
+    case os in
+        CENTOS | OPENSUSE)
+            ./bootstrap.sh
+            ./configure
+            make
+            ;;
+
+        UBUNTU)
+            # Do nothing
+            ;;
+
+        *)
+            echo -n "Unknown OS"
+           ;;
+
+    esac
+
 }
 
 install_griddb() {
@@ -93,5 +134,44 @@ stop_griddb() {
     local password=$2
     su -l gsadm -c "gs_stopcluster -u  $username/$password -w"
     su -l gsadm -c "gs_stopnode -u  $username/$password -w"
+}
+
+check_package() {
+    local os=$1
+
+    case os in
+        CENTOS | OPENSUSE)
+            rpm -qip installer/RPMS/x86_64/griddb-*-linux.x86_64.rpm
+            ;;
+
+        UBUNTU)
+            dpkg -i ../griddb_*_amd64.deb
+            ;;
+
+        *)
+            echo -n "Unknown OS"
+            ;;
+
+    esac
+}
+
+uninstall_package() {
+    local package_name=$1
+    local os=$2
+
+    case os in
+        CENTOS | OPENSUSE)
+            rpm -e $package_name
+            ;;
+
+        UBUNTU)
+            dpkg -r $package_name
+            ;;
+
+        *)
+            echo -n "Unknown OS"
+            ;;
+
+    esac
 }
 
